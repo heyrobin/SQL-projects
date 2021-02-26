cf1c25cad0a3d0cec3c733510a4670756b205388
Robin Singh <74088063+heyrobin@users.noreply.github.com>
2021-02-26 18:52:06 +0530

    Create Movie Actor dump.sql

---
+++ Movie Actor dump.sql
@@ -0,0 +1,45 @@
+/* Create table about the people and what they do here */
+CREATE TABLE famous(Name TEXT,net_worth TEXT,age INTEGER,location TEXT);
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES("Merv Griffin","$1 Billion",95, "United states");
+INSERT INTO famous (Name , net_worth, age, location) VALUES("Shahrukh Khan","$750 Million",54, "India");
+INSERT INTO famous (Name , net_worth, age, location) VALUES
+("Tom Cruise",	"$550 Million",	57	,"United states");
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("George Clooney",	"$500 Million",	58,	"United states");
+
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES	
+("Amitabh Bachchan",	"$425 Million",	77,	"India");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Mel Gibson","$425 Million",64 , "Australia");	
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Adam Sandler","$420 Million",53 , "United states");				
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Jack Nicholson","$400 Million",82 , "United states");								
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Sylvester Stallone","$400 Million",73 , "United states");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Jackie Chan","$395 Million",65 , "CHINA");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Clint Eastwood","$375 Million",89 , "United states");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Tom Hanks","$350 Million",63 , "United states");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Will Smith","$350 Million",51 , "United states");							
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Michael Douglas","$300 Million",75 , "United states");							
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Arnold Schwarzenegger","$300 Million",72 , "United states");								
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Robert Downey Jr.","$300 Million",54, "United states");			
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Patrick Wilson","$275 Million",46 , "United states");										
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Scott Speedman","$245 Million",44 , "Canada");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Leon Thomas","$245 Million",26 , "United states");					
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Leonardo DiCaprio","$245 Million",45 , "United states");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Brad Pitt","$240 Million",56 , "United states");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Salman Khan","$230 Million",54 , "India");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Harrison Ford","$230 Million",78 , "United states");					
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Mark Wahlberg","$225 Million",48 , "United states");
+
+INSERT INTO famous (Name , net_worth, age, location) VALUES			("Michael Shanks","$215 Million",49 , "Canada");
+
+
